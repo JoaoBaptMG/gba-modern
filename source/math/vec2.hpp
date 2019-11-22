13aab2a8646891cbe8befe78fc87d3aacacde4e1
João Baptista de Paula e Silva <jbaptistapsilva@yahoo.com.br>
2019-11-22 23:28:36 +0100

    Make vec2 constexpr

--- source/math/vec2.hpp
+++ source/math/vec2.hpp
@@ -15,77 +15,77 @@ struct vec2 final
     T x, y;
 
     // Default constructors
-    vec2() = default;
-    vec2(const vec2&) = default;
-    vec2(vec2&&) = default;
-    vec2& operator=(const vec2&) = default;
-    vec2& operator=(vec2&&) = default;
+    constexpr vec2() = default;
+    constexpr vec2(const vec2&) = default;
+    constexpr vec2(vec2&&) = default;
+    constexpr vec2& operator=(const vec2&) = default;
+    constexpr vec2& operator=(vec2&&) = default;
 
     // Normal constructor
-    vec2(T x, T y) : x(x), y(y) {}
+    constexpr vec2(T x, T y) : x(x), y(y) {}
 
     // Assignment operators
     template <typename U>
-    vec2& operator+=(vec2<U> o) { x += o.x; y += o.y; return *this; }
+    constexpr vec2& operator+=(vec2<auto> o) { x += o.x; y += o.y; return *this; }
     template <typename U>
-    vec2& operator-=(vec2<U> o) { x -= o.x; y -= o.y; return *this; }
+    constexpr vec2& operator-=(vec2<U> o) { x -= o.x; y -= o.y; return *this; }
     template <typename U>
-    vec2& operator&=(vec2<U> o) { x &= o.x; y &= o.y; return *this; }
+    constexpr vec2& operator&=(vec2<U> o) { x &= o.x; y &= o.y; return *this; }
     template <typename U>
-    vec2& operator|=(vec2<U> o) { x |= o.x; y |= o.y; return *this; }
+    constexpr vec2& operator|=(vec2<U> o) { x |= o.x; y |= o.y; return *this; }
     template <typename U>
-    vec2& operator^=(vec2<U> o) { x ^= o.x; y ^= o.y; return *this; }
+    constexpr vec2& operator^=(vec2<U> o) { x ^= o.x; y ^= o.y; return *this; }
 
     template <typename U>
-    vec2& operator*=(U v) { x *= v; y *= v; return *this; }
+    constexpr vec2& operator*=(U v) { x *= v; y *= v; return *this; }
     template <typename U>
-    vec2& operator/=(U v) { x /= v; y /= v; return *this; }
+    constexpr vec2& operator/=(U v) { x /= v; y /= v; return *this; }
     template <typename U>
-    vec2& operator%=(U v) { x %= v; y %= v; return *this; }
+    constexpr vec2& operator%=(U v) { x %= v; y %= v; return *this; }
 
-    vec2& operator<<=(int n) { x <<= n; y <<= n; return *this; }
-    vec2& operator>>=(int n) { x >>= n; y >>= n; return *this; }
+    constexpr vec2& operator<<=(int n) { x <<= n; y <<= n; return *this; }
+    constexpr vec2& operator>>=(int n) { x >>= n; y >>= n; return *this; }
 
     // Unary operators
-    vec2 operator~() const { return vec2(~x, ~y); }
-    vec2 operator-() const { return vec2(-x, -y); }
+    constexpr vec2 operator~() const { return vec2(~x, ~y); }
+    constexpr vec2 operator-() const { return vec2(-x, -y); }
 
     // Conversion operators
     template <typename U, typename = std::enable_if_t<!std::is_same_v<T, U> && std::is_convertible_v<T, U>>, typename = void>
-    operator vec2<U>() const { return vec2<U>(x, y); }
+    constexpr operator vec2<U>() const { return vec2<U>(x, y); }
     template <typename U, typename = std::enable_if_t<!std::is_same_v<T, U> && is_explicitly_convertible_v<T, U>>>
-    explicit operator vec2<U>() const { return vec2<U>(U(x), U(y)); }
+    constexpr explicit operator vec2<U>() const { return vec2<U>(U(x), U(y)); }
 
     // Utilities
-    auto dot(vec2 o) const { return x*o.x + y*o.y; }
-    auto cross(vec2 o) const { return x*o.y - y*o.x; }
+    constexpr auto dot(vec2 o) const { return x*o.x + y*o.y; }
+    constexpr auto cross(vec2 o) const { return x*o.y - y*o.x; }
 };
 
 // Template deduction guide
-template <typename Ty> vec2(Ty x, Ty y) -> vec2<Ty>;
+template <typename T> vec2(T x, T y) -> vec2<T>;
 
 // Free function operators
 template <typename T, typename U>
-auto operator+(vec2<T> a, vec2<U> b) { return vec2(a.x + b.x, a.y + b.y); }
+constexpr auto operator+(vec2<T> a, vec2<U> b) { return vec2(a.x + b.x, a.y + b.y); }
 template <typename T, typename U>
-auto operator-(vec2<T> a, vec2<U> b) { return vec2(a.x - b.x, a.y - b.y); }
+constexpr auto operator-(vec2<T> a, vec2<U> b) { return vec2(a.x - b.x, a.y - b.y); }
 template <typename T, typename U>
-auto operator&(vec2<T> a, vec2<U> b) { return vec2(a.x & b.x, a.y & b.y); }
+constexpr auto operator&(vec2<T> a, vec2<U> b) { return vec2(a.x & b.x, a.y & b.y); }
 template <typename T, typename U>
-auto operator|(vec2<T> a, vec2<U> b) { return vec2(a.x | b.x, a.y | b.y); }
+constexpr auto operator|(vec2<T> a, vec2<U> b) { return vec2(a.x | b.x, a.y | b.y); }
 template <typename T, typename U>
-auto operator^(vec2<T> a, vec2<U> b) { return vec2(a.x ^ b.x, a.y ^ b.y); }
+constexpr auto operator^(vec2<T> a, vec2<U> b) { return vec2(a.x ^ b.x, a.y ^ b.y); }
 
 template <typename T, typename U>
-auto operator*(vec2<T> v, U s) { return vec2(v.x * s, v.y * s); }
+constexpr auto operator*(vec2<T> v, U s) { return vec2(v.x * s, v.y * s); }
 template <typename T, typename U>
-auto operator*(T s, vec2<U> v) { return vec2(s * v.x, s * v.y); }
+constexpr auto operator*(T s, vec2<U> v) { return vec2(s * v.x, s * v.y); }
 template <typename T, typename U>
-auto operator/(vec2<T> v, U s) { return vec2(v.x / s, v.y / s); }
+constexpr auto operator/(vec2<T> v, U s) { return vec2(v.x / s, v.y / s); }
 template <typename T, typename U>
-auto operator%(vec2<T> v, U s) { return vec2(v.x % s, v.y % s); }
+constexpr auto operator%(vec2<T> v, U s) { return vec2(v.x % s, v.y % s); }
 
 template <typename T>
-auto operator<<(vec2<T> v, int n) { return vec2(v.x << n, v.y << n); }
+constexpr auto operator<<(vec2<T> v, int n) { return vec2(v.x << n, v.y << n); }
 template <typename T>
-auto operator>>(vec2<T> v, int n) { return vec2(v.x >> n, v.y >> n); }
+constexpr auto operator>>(vec2<T> v, int n) { return vec2(v.x >> n, v.y >> n); }
