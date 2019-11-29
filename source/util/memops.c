fec13e80d539f3ec4efc2517737e0cd8768900ec
João Baptista de Paula e Silva <jbaptistapsilva@yahoo.com.br>
2019-11-29 17:19:47 +0100

    A better algorithm for memmove

--- source/util/memops.c
+++ source/util/memops.c
@@ -9,7 +9,7 @@
 void* memcpy(void* dst, const void* src, size_t cnt)
 {
     // Return early
-    if (dst == src) return dst;
+    if (dst == src || cnt == 0) return dst;
 
     unsigned char* dst8 = (unsigned char*)dst;
     const unsigned char* src8 = (const unsigned char*)src;
@@ -52,6 +52,8 @@ void* memcpy(void* dst, const void* src, size_t cnt)
 
 void* memset(void* dst, int val, size_t cnt)
 {
+    // Return early
+    if (cnt == 0) return dst;
     unsigned char* dst8 = (unsigned char*)dst;
 
     // Truncate val to 8 bits
@@ -85,10 +87,20 @@ void* memmove(void* dst, const void* src, size_t cnt)
     // Call memcpy if the regions only overlap in the "good sense"
     if (dst8 <= src8 || dst8 >= src8+cnt) return memcpy(dst, src, cnt);
 
-    // Do a very dumb backwards copy
+    // Do "chunk calls" to memcpy
+    size_t ofs = dst8 - src8;
     dst8 += cnt;
     src8 += cnt;
-    while (cnt--) *dst8-- = *src8--;
 
+    while (cnt >= ofs)
+    {
+        dst8 -= ofs;
+        src8 -= ofs;
+        cnt -= ofs;
+        memcpy(dst8, src8, ofs);
+    }
+
+    // Copy the last chunk
+    memcpy(dst8-cnt, src8-cnt, cnt);
     return dst;
 }
