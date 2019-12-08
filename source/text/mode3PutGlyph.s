538e586e3c1d588d90e919f673b5da648c44681d
João Baptista de Paula e Silva <jbaptistapsilva@yahoo.com.br>
2019-12-08 14:42:37 +0100

    Reworked comments

--- source/text/mode3PutGlyph.s
+++ source/text/mode3PutGlyph.s
@@ -5,16 +5,16 @@
 @--------------------------------------------------------------------------------
 
 @ for reference:
-@struct GlyphData final
-@{
-@    const char* data; // 1bpp data
-@    std::uint8_t height;
-@    std::int8_t offsetX;
-@    std::int8_t offsetY;
-@    std::uint8_t advanceX:7;
+@ struct GlyphData final
+@ {
+@     const char* data; // 1bpp data
+@     std::uint8_t height;
+@     std::int8_t offsetX;
+@     std::int8_t offsetY;
+@     std::uint8_t advanceX:7;
 @
-@    // 0 for 16-bit bitfield, 1 for 32-bit bitfield
-@    std::uint8_t attr:1;
+@     // 0 for 16-bit bitfield, 1 for 32-bit bitfield
+@     std::uint8_t attr:1;
 @ };
 
 @ void mode3PutGlyph(int x, int y, const GlyphData* gl, COLOR color, void* buffer);
@@ -32,7 +32,7 @@ mode3PutGlyph:
 
     mov     r2, r5, lsl #16         @ Last byte will contain gl->offsetX
     add     r0, r0, r2, asr #24     @ Sign extend gl->offsetX and add to x
-    mov     r2, r5, lsl #8          @ Last byte will contain gl->offsetX
+    mov     r2, r5, lsl #8          @ Last byte will contain gl->offsetY
     add     r1, r1, r2, asr #24     @ Sign extend gl->offsetY and add to y
 
     @ Calculate the first position in space
