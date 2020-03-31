acc86e080be6bbb6b3f6c66251df0b4cb230ab3e
João Baptista de Paula e Silva <jbaptistapsilva@yahoo.com.br>
2020-03-31 18:13:58 -0300

    Add random utilities

---
+++ source/util/random.niwram.c
@@ -0,0 +1,16 @@
+//--------------------------------------------------------------------------------
+// random.c
+//--------------------------------------------------------------------------------
+// Provides a simple random number generator
+//--------------------------------------------------------------------------------
+#include "random.h"
+
+unsigned long long randomState IWRAM_DATA;
+u32 random()
+{
+    randomState ^= randomState >> 11;
+    randomState ^= randomState << 31;
+    randomState ^= randomState >> 18;
+    u32 result = (randomState * 0xd989bcacc137dcd5ULL) >> 32;
+    return result;
+}
