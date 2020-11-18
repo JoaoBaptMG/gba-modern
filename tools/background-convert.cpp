3ad32c9172f32b7bf851273e20d85576100656f2
Xett <ethan.jones@my.jcu.edu.au>
2020-11-19 03:16:38 +1000

    Update background-convert.cpp

--- tools/background-convert.cpp
+++ tools/background-convert.cpp
@@ -12,7 +12,7 @@ auto setUnion(const std::vector<Color>& s1, const std::vector<Color>& s2)
     return sresult;
 }
 
-ConversionResult convertBackgroundTo4bpp(const State<Character8bpp>& state8bpp,
+ConversionResult convertBackgroundTo4bpp(const BackgroundState<Character8bpp>& state8bpp,
     const std::array<Color, 256>& originalPalette)
 {
     // To begin with, we are going to build a set of all colors a tile uses
@@ -96,7 +96,7 @@ ConversionResult convertBackgroundTo4bpp(const State<Character8bpp>& state8bpp,
     }
 
     // Finally, we begin to construct the new state
-    State<Character4bpp> state;
+    BackgroundState<Character4bpp> state;
     std::vector<std::size_t> charCorrespondences(numChars);
 
     // Put the new characters first
