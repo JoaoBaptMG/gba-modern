12caef4ef0d0063c762426a59f51e71a1d4cde9b
Xett <ethan.jones@my.jcu.edu.au>
2020-11-20 02:55:57 +1000

    Toolchain works, building resources with commands, placeholder targets to plan out build order

--- toolchain.cmake
+++ toolchain.cmake
@@ -1,20 +1,9 @@
-set(CMAKE_SYSTEM_NAME Linux)
-set(CMAKE_SYSTEM_PROCESSOR arm)
+set(CMAKE_SYSTEM_NAME Generic)
+set(CMAKE_SYSTEM_PROCESSOR ARM)
+set(CMAKE_SYSTEM_VERSION 1)
+set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
 
-# Path to the tools used
-set(ARMCC ${CMAKE_SOURCE_DIR}/gcc/bin/arm-none-eabi-gcc CACHE PATH "ARMCC")
-#ARMCC := gcc/bin/arm-none-eabi-gcc
-set(ARMCPP ${CMAKE_SOURCE_DIR}/gcc/bin/arm-none-eabi-g++ CACHE PATH "ARMCPP")
-#ARMCPP := gcc/bin/arm-none-eabi-g++
-set(ARMOC ${CMAKE_SOURCE_DIR}/gcc/bin/arm-none-eabi-objcopy CACHE PATH "ARMOC")
-#ARMOC := gcc/bin/arm-none-eabi-objcopy
-set(ARMOD ${CMAKE_SOURCE_DIR}/gcc/bin/arm-none-eabi-objdump CACHE PATH "ARMOD")
-#ARMOD := gcc/bin/arm-none-eabi-objdump
-set(ARMLD ${ARMCPP})
-
-set(CMAKE_C_COMPILER ${ARMCC})
-set(CMAKE_CXX_COMPILER ${ARMCPP})
-set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
-set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
-set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
-set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
+set(CMAKE_C_COMPILER ${CMAKE_SOURCE_DIR}/gcc/bin/arm-none-eabi-gcc)
+set(CMAKE_CXX_COMPILER ${CMAKE_SOURCE_DIR}/gcc/bin/arm-none-eabi-g++)
+set(CMAKE_OBJCOPY ${CMAKE_SOURCE_DIR}/gcc/bin/arm-none-eabi-objcopy)
+set(CMAKE_OBJDUMP ${CMAKE_SOURCE_DIR}/gcc/bin/arm-none-eabi-objdump)
