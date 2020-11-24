64c922478fceb57e5969b6e66245d893c5690d0e
Xett <ethan.jones@my.jcu.edu.au>
2020-11-25 01:13:38 +1000

    Big refactor

--- toolchain.cmake
+++ toolchain.cmake
@@ -7,3 +7,11 @@ set(CMAKE_C_COMPILER ${CMAKE_SOURCE_DIR}/external/gcc/bin/arm-none-eabi-gcc)
 set(CMAKE_CXX_COMPILER ${CMAKE_SOURCE_DIR}/external/gcc/bin/arm-none-eabi-g++)
 set(CMAKE_OBJCOPY ${CMAKE_SOURCE_DIR}/external//gcc/bin/arm-none-eabi-objcopy)
 set(CMAKE_OBJDUMP ${CMAKE_SOURCE_DIR}/external/gcc/bin/arm-none-eabi-objdump)
+set(CMAKE_EXECUTABLE_SUFFIX .elf)
+
+message(STATUS
+	"CMAKE_C_COMPILER - ${CMAKE_C_COMPILER}\n"
+	"CMAKE_CXX_CCOMPILER - ${CMAKE_CXX_COMPILER}\n"
+	"CMAKE_OBJCOPY - ${CMAKE_OBJCOPY}\n"
+	"CMAKE_OBJDUMP - ${CMAKE_OBJDUMP}\n"
+	"CMAKE_EXECUTABLE_SUFFIX - ${CMAKE_EXECUTABLE_SUFFIX}\n")
