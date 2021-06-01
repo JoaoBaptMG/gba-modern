b9862d92b903f73dca50c0aa3d310d1453584362
João Baptista de Paula e Silva <jbaptistapsilva@yahoo.com.br>
2021-06-01 10:45:10 +0200

    Change split's iterator category

--- tools/split.hpp
+++ tools/split.hpp
@@ -25,7 +25,7 @@ public:
         using value_type = sview;
         using pointer = const sview*;
         using reference = sview;
-        using iterator_category = std::input_iterator_tag;
+        using iterator_category = std::forward_iterator_tag;
 
         iterator(const basic_string_splitter& splitter, stype lastpos, stype pos)
             : splitter(splitter), lastpos(lastpos), pos(pos) {}
